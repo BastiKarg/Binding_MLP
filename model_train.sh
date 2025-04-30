a964ef97d45d8aaacb9aa96a0dbb2077005fb6cb
Sebastian Karg <cakesebastian@gmail.com>
2025-04-30 14:26:27 +0200

    Add files via upload

--- model_train.sh
+++ model_train.sh
@@ -1,226 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=jobNoSco   # Job name
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --time=720:00:00   #720:00:00               # Time limit hrs:min:sec
-#SBATCH --mem=120gb     # Job memory request
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=56  # war 56 bei dem run 66318
-#SBATCH --output=CPU_Frish_single%j.log   # Standard output and error log
-#SBATCH --nodelist=gpunode09
-#SBATCH --partition=GPU-A40
 
+#This script can be used to train models with an training data input of interest. Adjust parameters accordingly 
 
-#GPU-A40      up   infinite      2  alloc gpunode[09-10]
+#set according to IT-infrastructure
+#SLURM_CPUS_PER_TASK=
 
+input=atlas_iedb # change if other data resource is used
 
-echo "Date              = $(date)"
-echo "Hostname          = $(hostname -s)"
-echo "Working Directory = $(pwd)"
-echo ""
-echo "Number of Nodes Allocated      = $SLURM_JOB_NUM_NODES"
-echo "Number of Tasks Allocated      = $SLURM_NTASKS"
-echo "Number of Cores/Task Allocated = $SLURM_CPUS_PER_TASK"
+# define range of seeds of interest
+start_seed=1
+end_seed=20
 
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/etc/profile.d/conda.sh" ]; then
-        . "/etc/profile.d/conda.sh"
-    else
-        export PATH="/usr/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-
-
-#echo "Running MLP training on the bench dataset of expitope from the hlaLigandAtlas and Protrans and focusing on f1 value of strong with sweep"
-
-
-echo "Sweeping that sweet sweep sweatily"
-echo ""
-export LD_LIBRARY_PATH=/home/students/s.karg/.conda/envs/py39_gpu/lib/
-export CUDA_VISIBLE_DEVICES=0
-
-#source /home/students/s.karg/.bashrc
-#CONDA_EXE=/usr/bin/conda
-
-#$CONDA_EXE update conda -y
-#$CONDA_EXE init
-
-#source /home/students/s.karg/.bashrc
-#conda init bash
-conda activate py39_gpu
-
-#python -m   pip install cudann
-
-#wandb login --relogin
-#wandb online
-
-
-cd /proj/Expitope/BindingMLP-main
-
-dir=splits_atlas_iedb/
-dir=splits_atlas_protransonly #splits_iedb_protransonly splits_atlas_iedb_protransonly
-
-#for data in atlas atlas_iedb iedb; do sbatch train_without_scores_frishman.sh $data ; done 
-
-
-data=$1	
-dir=splits_${data}_protransonly
-
-
-
-
-#for data in atlas atlas_iedb atlas_iedb_final; do 
-for data in atlas_iedb; do
-
-
-dir=splits_${data}_under
-
-
-echo "dir ist $dir"
-echo "data is $data"
-
-arch=gpu
-
-#for i in {2..20} ; do
-for i in {1..20} ; do
-	cd /proj/Expitope/BindingMLP-main/data
-	 mkdir -p splits_${data}_under/split_$i
-
-	if [ ! -f splits_${data}_under/split_$i/val_data.tsv ]; then
-		python splitter_${data}_under.py $i
+for seed in {${start_seed}..${end_seed}} ; do 
+	cd data
+	mkdir -p splits_${input}_under/split_$seed
+	if [ ! -f splits_${input}_under/split_$seed/val_data.tsv ]; then
+		python splitter.py $seed $input
 	fi
-        cd /proj/Expitope/BindingMLP-main
+	cd ..
+	python train.py --val_path data/$input/split_${seed}/val_data.tsv --train_path data/$input/split_${seed}/train_data.tsv --test_path data/$input/split_${seed}/test_data.tsv
+	# add the following parameters to achieve best performing model
+	#--num_workers $SLURM_CPUS_PER_TASK --batch_size 256 --learning_rate 0.0000539 --weight_decay 0.0001262 --num_layers 11 --dropout_prob 0.005113 --init_type kaiming  --num_nodes_per_layer 991
 
-	python train_${data}_under_${arch}.py --val_path data/$dir/split_${i}/val_data.tsv --train_path data/$dir/split_${i}/train_data.tsv --test_path data/$dir/split_${i}/test_data.tsv  --num_workers $SLURM_CPUS_PER_TASK --batch_size 256 --learning_rate 0.0000539 --weight_decay 0.0001262 --num_layers 11 --dropout_prob 0.005113 --init_type kaiming  --num_nodes_per_layer 991
-
-	#python train_fix_atlas_iedb_v2_sweepOnly.py --val_path data/$dir/split_${i}/val_data.tsv --train_path data/$dir/split_${i}/train_data.tsv --test_path data/$dir/split_${i}/test_data.tsv  --num_workers 1 --tune
-
-	#python train_fix_atlas_iedb_v2_sweepOnly.py --val_path data/$dir/split_${i}/val_data.tsv --train_path data/$dir/split_${i}/train_data.tsv --test_path data/$dir/split_${i}/test_data.tsv  --num_workers 1 --tune
-	#wandb agent bioinformatics_freising/uncategorized/kas7fhx0  # no protrans split 7 
-       	#wandb agent bioinformatics_freising/uncategorized/nv7ca1bc  #506
-
-	#wandb agent bioinformatics_freising/uncategorized/l0j7dwso  #354
-	#wandb agent bioinformatics_freising/uncategorized/b32hucha
-	#rm data/$dir/split_${i}/*
 done
-#done
-
-
-done # for data
-
-exit 1 
-
-
-#CUDA_VISIBLE_DEVICES= "1" python -m torch.distributed.launch python train.py --val_path data/val_data.tsv --train_path data/train_data.tsv --test_path data/test_data.tsv  --num_workers 8 --batch_size 512
-
-#for i in {4..20} ; do
-#for i in {1..100} ; do
-#wandb sweep sweep.yaml
-
-
-#echo `sysctl kernel.shmall`
-#echo `sysctl kernel.shmmax`
-
-#exit 1 
-
-export LD_LIBRARY_PATH=/home/students/s.karg/.conda/envs/py39_gpu/lib/
-export CUDA_VISIBLE_DEVICES=0
-#python -c "import tensorflow as tf; print(tf.reduce_sum(tf.random.normal([1000, 1000])))"
-python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
-echo ""
-echo "BLIBLABLUB"
-echo ""
-python -c "import torch; print(torch.cuda.is_available())"
-
-#exit 1 
-#nvidia-smi
-
-#python gputest.py
-#exit 1
-
-wandb agent sebastian-karg/sweep/wbl613gy
-wand sweep --resume  sebastian-karg/sweep/wbl613gy
-wandb agent sebastian-karg/sweep/wbl613gy
-wand sweep --resume  sebastian-karg/sweep/wbl613gy
-wandb agent sebastian-karg/sweep/wbl613gy
-wand sweep --resume  sebastian-karg/sweep/wbl613gy
-wandb agent sebastian-karg/sweep/wbl613gy
-wand sweep --resume  sebastian-karg/sweep/wbl613gy
-wandb agent sebastian-karg/sweep/wbl613gy
-wand sweep --resume  sebastian-karg/sweep/wbl613gy
-
-exit 1 
-
-wandb agent sebastian-karg/sweep/q04qkxe3
-wandb sweep --resume sebastian-karg/sweep/q04qkxe3
-wandb agent sebastian-karg/sweep/q04qkxe3
-wandb sweep --resume sebastian-karg/sweep/q04qkxe3
-wandb agent sebastian-karg/sweep/q04qkxe3
-wandb sweep --resume sebastian-karg/sweep/q04qkxe3
-wandb agent sebastian-karg/sweep/q04qkxe3
-wandb sweep --resume sebastian-karg/sweep/q04qkxe3
-wandb agent sebastian-karg/sweep/q04qkxe3
-wandb sweep --resume sebastian-karg/sweep/q04qkxe3
-
-exit 1 
-
-wandb agent sebastian-karg/sweep/m2cjp74y
-wandb sweep --resume sebastian-karg/sweep/m2cjp74y
-wandb agent sebastian-karg/sweep/m2cjp74y
-wandb sweep --resume sebastian-karg/sweep/m2cjp74y
-wandb agent sebastian-karg/sweep/m2cjp74y
-wandb sweep --resume sebastian-karg/sweep/m2cjp74y
-wandb agent sebastian-karg/sweep/m2cjp74y
-#wandb agent sebastian-karg/sweep/bhl2z2o1
-#wandb agent sebastian-karg/sweep/3k6vlsjc
-
-#wandb agent sebastian-karg/sweep/pl7l9zem
-
-exit 1
-
-
-
-for i in 10 ; do #95 best one, before 10 1  
-  #cd /home/students/s.karg/work/bindmlp/BindingMLP-main/data
-  #python splitter_atlas_iedb_v2.py $i
-  #cd /home/students/s.karg/work/bindmlp/BindingMLP-main
-
-#    for lr in 1e-5  3e-7 ; do # hier einmal anders herum gestartet, weil abgebrochen 
-#for lr in 3e-5 1e-5 3e-6 1e-6 3e-7 1e-7 ; do   
-#      for wd in  1e-6 1e-4 0; do 
-#         for nl in   2 4 ; do 
-			#for nnpl in 128 512 ; do 
-#           for dp in  0.005 0.05; do 
-#	     for bs in 128 ; do
-              # for nnpl in 512 ; do # tried 128 but failed somehow
-#	         for it in normal xavier kaiming orthogonal; do 
-                   #cd /home/students/s.karg/work/bindmlp/BindingMLP-main
-		    python train_fix_atlas_iedb_v2_sweepOnly.py --val_path data/$dir/split_${i}/val_data.tsv --train_path data/$dir/split_${i}/train_data.tsv --test_path data/$dir/split_${i}/test_data.tsv  --num_workers 1 --tune
-#                   python train_fix_atlas_iedb_v2_sweepOnly.py --val_path data/$dir/split_${i}/val_data.tsv --train_path data/$dir/split_${i}/train_data.tsv --test_path data/$dir/split_${i}/test_data.tsv  --num_workers 16 --batch_size $bs --learning_rate $lr --weight_decay $wd --num_layers $nl --dropout_prob $dp --init_type $it #--num_nodes_per_layer $nnpl
-	           #rm data/$dir/split_${i}/*
-
-	        #rm data/splits_atlas_protrans/split_${i}/*
-#              done
-#            done
-#          done
-#        done
-#      done
-#    done
-#  done
-done
-#rm data/$dir/split_${i}/*
-wandb sync
-
-#python train.py --val_path data/val_data.tsv --train_path data/train_data.tsv --test_path data/test_data.tsv  --num_workers 8 --batch_size 512
-
-
 
 
---
+++ train.py
@@ -0,0 +1,172 @@
+import os
+import sys
+from argparse import ArgumentParser
+import torch
+from torch.utils.data import DataLoader
+import torch.multiprocessing
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, StochasticWeightAveraging, LearningRateMonitor
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+from model.MLP_f1_strong_dropout import MLP 
+from dataset.Dataset import csv_dataset
+import csv
+import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.preprocessing import LabelEncoder
+from collections import Counter
+from imblearn.over_sampling import SMOTE
+from pandas import read_csv
+
+
+def get_next_version(root_dir):
+  _fs = pl.utilities.cloud_io.get_filesystem(root_dir)
+
+  try:
+      listdir_info = _fs.listdir(root_dir)
+  except OSError:
+    print("Missing logger folder: %s", root_dir)
+    return 0
+
+  existing_versions = []
+  for listing in listdir_info:
+      d = listing["name"]
+      bn = os.path.basename(d)
+      if _fs.isdir(d) and bn.startswith("version_"):
+          dir_ver = bn.split("_")[1].replace("/", "")
+          existing_versions.append(int(dir_ver))
+  if len(existing_versions) == 0:
+      return 0
+
+  return max(existing_versions) + 1
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+torch.backends.cudnn.determinstic = True
+torch.backends.cudnn.benchmark = False
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+parser = ArgumentParser()
+parser.add_argument('--num_workers', type=int, default=1) # 58
+parser.add_argument('--batch_size', type=int, default=512)
+parser.add_argument('--val_check_n', type=int, default=5)
+parser.add_argument('--seed', type=int, default=42)
+parser.add_argument('--train_path', type=str)
+parser.add_argument('--val_path', type=str)
+parser.add_argument('--test_path', type=str)
+parser.add_argument('--num_classes', type=int, default=3)
+parser.add_argument('--checkpoint', type=str, default=None)
+parser.add_argument('--metric', type=str, default='f1.2')
+parser.add_argument('--test', action='store_true')
+parser.add_argument('--tune', default = False) # , action='store_true'
+parser = MLP.add_model_specific_args(parser) # CAUTION: STATIC METHOD. IF ADDING MODEL SPECIFIC ARGS, MUST BE DONE IN THIS CLASS
+parser = pl.Trainer.add_argparse_args(parser)
+hparams = parser.parse_args()
+
+pl.seed_everything(hparams.seed)
+
+model_input = hparams.val_path.split("/")[2]
+
+def train():
+  model_name = 'BindingMLP_' + model_input
+  log_dir = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])),'logs',model_name)
+  train_dataset = csv_dataset(hparams.train_path)
+  val_dataset = csv_dataset(hparams.val_path)
+
+  ## do the smote resampling
+  df = read_csv(hparams.train_path, sep='\t' )
+  data = df.values
+  X, y = data[:, :-1], data[:, -1]
+  #y = LabelEncoder().fit_transform(y)
+  counter = Counter(y)
+  new_sizes_groups = round(len(y)/3)
+  #undersample = RandomUnderSampler(sampling_strategy={0.0:new_sizes_groups, 1.0:counter[1], 2.0:counter[2]})
+  undersample = RandomUnderSampler(sampling_strategy={0.0:new_sizes_groups, 1.0:counter[1], 2.0:new_sizes_groups})
+
+  #undersample = RandomUnderSampler(sampling_strategy={0.0:new_sizes_groups, 1.0:counter[1], 2.0:new_sizes_groups})
+  X, y = undersample.fit_resample(X, y)
+  oversample = SMOTE(sampling_strategy={0.0:new_sizes_groups, 1.0:new_sizes_groups, 2.0:new_sizes_groups})
+  #oversample = SMOTE(sampling_strategy={0.0:new_sizes_groups, 1.0:new_sizes_groups, 2.0:new_sizes_groups})
+
+  X, y = oversample.fit_resample(X, y)
+
+  xpd = pd.DataFrame(X)
+  xpd['11'] = y.tolist()
+  with open(os.path.dirname(hparams.train_path) + '/train_data_balanced.tsv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(pd.DataFrame.to_numpy(xpd))
+  #train_dataset = xpd
+  train_dataset = csv_dataset(os.path.dirname(hparams.train_path) + '/train_data_balanced.tsv')
+  hparams.input_size=train_dataset.get_input_size()
+
+
+  del xpd, X, df, data
+
+
+  version = get_next_version(log_dir)
+  log_folder = os.path.join(log_dir,f'version_{version}')
+
+  callbacks = []
+  callbacks.append(ModelCheckpoint(monitor=f'val.{hparams.metric}',mode='max',filename=f'best_val_{hparams.metric}_{version}',dirpath=log_folder))
+  callbacks.append(LearningRateMonitor(logging_interval='epoch'))
+  callbacks.append(EarlyStopping(monitor=f'val.{hparams.metric}', min_delta=0.00, patience=20/hparams.val_check_n, verbose=False, mode='max'))
+
+  model = MLP(hparams)
+
+  train_loader = DataLoader(train_dataset,  num_workers=hparams.num_workers, batch_size=hparams.batch_size, pin_memory=True, persistent_workers=True, shuffle=True)
+  val_loader = DataLoader(val_dataset,  num_workers=hparams.num_workers, batch_size=hparams.batch_size, pin_memory=True, persistent_workers=True, shuffle=False)
+
+
+  if not hparams.checkpoint:
+    trainer = pl.Trainer.from_argparse_args(hparams, logger=logger, gpus=1, check_val_every_n_epoch=hparams.val_check_n, callbacks=callbacks, enable_progress_bar=True, max_epochs=hparams.max_epochs, accelerator='cpu')
+    trainer.fit(model, train_loader, val_loader)
+    logger.log_metrics({f'best.val.{hparams.metric}': model.best_val_metric})
+    ckpt_path = os.path.join(log_folder,f'best_val_{hparams.metric}_{model.best_val_metric}.ckpt')
+  else:
+    trainer = pl.Trainer.from_argparse_args(hparams, logger=logger, gpus=1, resume_from_checkpoint=hparams.checkpoint)
+    ckpt_path = hparams.checkpoint
+
+  # Test if needed
+  if hparams.test:
+    test_dataset = csv_dataset(hparams.test_path)
+    test_loader = DataLoader(test_dataset,  num_workers=hparams.num_workers, batch_size=hparams.batch_size, pin_memory=True, persistent_workers=True, shuffle=False)
+    trainer.test(model, test_loader, ckpt_path=ckpt_path)
+
+sweep_config = {
+    'method': 'bayes',
+    'metric': {
+      'name': 'val.f1.2',
+      'goal': 'maximize'
+    },
+    'parameters': {
+        'learning_rate': {
+            'values': [3e-5, 1e-5, 3e-6, 1e-6, 3e-7, 1e-7]
+        },
+        'weight_decay': {
+            'values': [1e-3, 1e-4, 1e-5, 1e-6, 0]
+        },
+        'num_layers': {
+            'values': [1, 2, 3, 4]
+        },
+        'num_nodes_per_layer': {
+            'values': [64, 128, 256, 512, 1024]
+        },
+        'dropout_prob': {
+            'values': [0.3, 0.2, 0.1, 0.05, 0.01, 0.005]
+        },
+        'init_type': {
+            'values': ['normal', 'xavier', 'kaiming', 'orthogonal']
+        }
+      }
+
+}
+
+logger = WandbLogger(project='BindinMLP_' + model_input)
+sweep_id = wandb.sweep(
+    sweep=sweep_config, 
+    project="BindinMLP_" + model_input)
+
+if hparams.tune:
+  wandb.agent(sweep_id, function=train)
+else:
+  train()
